8b2603cbb5d168cf79294dff589a364fdeb7ea5e
Lucas Machado <lucas.dpmachado@gmail.com>
2017-12-27 23:08:14 -0200

    Remove insecure package * Add new Meteor methods to links collection * Update main.js from client and server with links collection * Add valid-url package * Add event handler to input field from link_create component

---
+++ client/main.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import Header from './components/header';
+import LinkCreate from './components/link_create';
+import { Links } from '../imports/collections/links';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <LinkCreate />
+    </div>
+  )
+};
+
+Meteor.startup(() => {
+  ReactDOM.render(<App />, document.querySelector('.main'));
+});
--- server/main.js
+++ server/main.js
@@ -1,6 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 import _ from 'lodash';
 
+import { Links } from '../imports/collections/links';
+
 Meteor.startup(() => {
 
 });
